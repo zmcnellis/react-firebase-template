8a25bf016078d7d721db125f3c6e38ce3ec4b221
Zachary McNellis <zmcnell@g.clemson.edu>
2017-06-27 21:35:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components';
+import './index.css';
+import './favicon.ico';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
